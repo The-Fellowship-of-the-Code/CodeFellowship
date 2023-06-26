244cbaf26c674ff766bc1a556b17a8e4487ec95a
Ryan Gosselin <ryan.gosselin5@gmail.com>
2023-06-26 21:27:11 +0000

    basic file structure

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        {/* <Router>
+          <Header />
+          <Routes>
+            <Route 
+              exact path="/"
+              element={<BestStories />}
+            >
+            </Route>
+            <Route
+              exact path="/about"
+              element = {<Profile />}
+            >
+
+            </Route>
+          </Routes>
+          <Footer />
+        </Router> */}
+    </>
+  );
+}
+
+export default App;
