c08b151f4898db6ecb3337e554c8e7f125337d10
Ryan Gosselin <ryan.gosselin5@gmail.com>
2023-06-27 18:49:01 +0000

    have a button to add and the form is populating in the console

---
+++ src/StoryForm.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Button, Modal, Form } from 'react-bootstrap';
+
+class StoryForm extends React.Component {
+
+
+  handleStorySubmit = (event) => {
+    event.preventDefault();
+    let storyObj = {
+      title: event.target.title.value,
+      age: event.target.age.value,
+      mainChar: event.target.mainChar.value,
+      problem: event.target.problem.value,
+      setting: event.target.setting.value
+    }
+    this.props.handleCloseModal();
+    // this.props.postStory(storyObj);
+    console.log(storyObj);
+  }
+
+
+
+  render() {
+    return (
+      <>
+
+        <Modal show={this.props.show} onHide={this.props.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a Story</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleStorySubmit} className="storyForm">
+              <Form.Group controlId="title">
+                <Form.Label>Provide a Title For Your Story</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="age">
+                <Form.Label>Age</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="mainChar">
+                <Form.Label>Main Character</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="problem">
+                <Form.Label>Problem</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group controlId="setting">
+                <Form.Label>Setting/Location</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Button type="submit">Create</Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
+
+
+
+
+
+
+
+
+}
+
+export default StoryForm;
